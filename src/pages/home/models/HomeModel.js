f19566dfcf360ae608a1391cce0b6606a96fc8e0
CookieJ <cookiejlim@gmail.com>
2017-12-21 13:00:15 +0800

    Use types.frozen instead of Group

--- src/pages/home/models/HomeModel.js
+++ src/pages/home/models/HomeModel.js
@@ -7,39 +7,32 @@
  */
 import {types, flow} from 'mobx-state-tree';
 
-const Category = types.model('Category', {
-  id: types.number,
-  name: types.string,
-  image_url: types.string,
-});
-
-const Group = types.model('Group', {
-  kind: types.string,
-  categories: types.optional(types.array(Category), [])
-});
-
 const Home = types.model('Home', {
-  isLoading: true,
-  isLoadError: false,
-  foodList: types.optional(types.array(Group), []),
-}).actions(self => ({
+  foodList: types.optional(types.array(types.frozen), []),
+  state: types.optional(types.enumeration(['loading', 'loadSuccess', 'loadError']), 'loading'),
+}).views(self => ({
+  get isLoading() {
+    return self.state === 'loading';
+  },
+  get isLoadError() {
+    return self.state === 'loadError';
+  }
+})).actions(self => ({
   fetchData: flow(function*() {
     try {
       const url = 'http://food.boohee.com/fb/v1/categories/list';
       const responseData = yield fetch(url).then(res => res.json());
       self.foodList = responseData.group;
-      self.isLoading = false;
+      self.state = 'loadSuccess';
     } catch (e) {
-      self.isLoadError = true;
-      self.isLoading = false;
+      self.state = 'loadError';
       console.log(`[Home] fetch category list error: ${e}`);
     }
   }),
   refresh: () => {
-    self.isLoading = true;
-    self.isLoadError = false;
+    self.state = 'loading';
     self.fetchData();
-  }
+  },
 }));
 
 export default {
