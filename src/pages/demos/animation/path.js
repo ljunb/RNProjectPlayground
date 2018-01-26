b96f45f851d4858bfa5411f5d5c969fc02731b31
cookiej <cookiejlim@gmail.com>
2018-01-26 10:26:14 +0800

    Path demo: Add animation for clicking menu item

--- src/pages/demos/animation/path.js
+++ src/pages/demos/animation/path.js
@@ -12,8 +12,10 @@ import {
   Animated,
   Text,
 } from 'react-native';
-import PropTypes from 'prop-types';
+import PropTypes, { element } from 'prop-types';
 
+const MenuItemWH = 30;
+const SwitchBtnWH = 40;
 const styles = StyleSheet.create({
   root: {
     flex: 1,
@@ -24,35 +26,50 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 40,
-    width: 40,
-    borderRadius: 20,
+    height: SwitchBtnWH,
+    width: SwitchBtnWH,
+    borderRadius: SwitchBtnWH / 2,
   },
   item: {
-    height: 30,
-    width: 30,
-    borderRadius: 15,
+    height: MenuItemWH,
+    width: MenuItemWH,
+    borderRadius: MenuItemWH / 2,
     backgroundColor: 'red',
     position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   itemText: {
     backgroundColor: 'transparent',
     color: '#fff',
+  },
+  menuBtn: {
+    height: MenuItemWH,
+    width: MenuItemWH,
+    borderRadius: MenuItemWH / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 export default class PathMenuAnimation extends Component {
   static propTypes = {
     icons: PropTypes.array,
+    radius: PropTypes.number,
+    itemWidth: PropTypes.number,
+    switchButtonWidth: PropTypes.number,
+    animationScale: PropTypes.number,
+    onSelect: PropTypes.func,
   };
 
   static defaultProps = {
     icons: [0, 1, 2, 3, 4],
+    radius: 120,
+    itemWidth: 30,
+    switchButtonWidth: 40,
+    animationScale: 1.5,
   };
 
   animatedValue = new Animated.Value(0);
+  clickedValue = new Animated.Value(0);
   isFold = true;
   state = {
     origin: {
@@ -61,11 +78,12 @@ export default class PathMenuAnimation extends Component {
       width: 0,
       height: 0,
     },
+    clickMenu: null,
   };
 
   handleShowOrHideMenu = () => {
     this.actionBtn && this.actionBtn.measure((ox, oy, width, height, px, py) => {
-      this.setState({origin: {width, height, x: px, y: py}}, () => {
+      this.setState({ origin: { width, height, x: px, y: py } }, () => {
         if (this.isFold) {
           this.spreadMenus();
         } else {
@@ -102,20 +120,50 @@ export default class PathMenuAnimation extends Component {
     ]).start(() => this.isFold = true);
   };
 
+  hanldePressItem = index => {
+    this.setState({ clickMenu: index }, () => {
+      Animated.parallel([
+        Animated.timing(this.clickedValue, {
+          toValue: 1,
+          duration: 450,
+        }),
+        Animated.sequence([
+          Animated.timing(this.animatedValue, {
+            toValue: 1.3,
+            duration: 150,
+          }),
+          Animated.timing(this.animatedValue, {
+            toValue: 0,
+            duration: 300,
+          }),
+        ])
+      ]).start(() => {
+        this.props.onSelect && this.props.onSelect(index);
+        this.resetToDefaultConfig();
+      });
+    });
+  };
+
+  resetToDefaultConfig = () => {
+    this.isFold = true;
+    this.setState({ clickMenu: null }, () => this.clickedValue.setValue(0));
+  };
+
   get buildAnimatedValues() {
-    const {icons} = this.props;
+    const { icons, itemWidth, radius } = this.props;
     // 每个按钮之间的间距角度
-    const marginAngle = Math.PI / 180 * 30;
-    const restAngle = (Math.PI / 180 * 180 - (icons.length - 1) * 30 * Math.PI / 180) / 2;
+    const marginAngle = Math.PI / 180 * itemWidth;
+    const restAngle = (Math.PI / 180 * 180 - (icons.length - 1) * itemWidth * Math.PI / 180) / 2;
     return icons.map((icon, index) => {
-      const top = -120 * Math.sin(restAngle + index * marginAngle) ;
-      const left = 120 * Math.cos(restAngle + index * marginAngle);
-      return {top, left};
+      const top = -radius * Math.sin(restAngle + index * marginAngle);
+      const left = radius * Math.cos(restAngle + index * marginAngle);
+      return { top, left };
     });
   }
 
   render() {
-    const {origin} = this.state;
+    const { itemWidth, switchButtonWidth, animationScale } = this.props;
+    const { origin } = this.state;
 
     const rotate = this.animatedValue.interpolate({
       inputRange: [0, 1],
@@ -130,26 +178,48 @@ export default class PathMenuAnimation extends Component {
       <View style={styles.root}>
         {this.props.icons.map((icon, index) => {
           const animatedItem = this.buildAnimatedValues[index];
-          const top = this.animatedValue.interpolate({
+          const isClicked = this.state.clickMenu === index;
+
+          let top = this.animatedValue.interpolate({
             inputRange: [0, 1],
-            outputRange: [origin.y + 5, originCenter.y + animatedItem.top],
+            outputRange: [origin.y + (switchButtonWidth - itemWidth) / 2, originCenter.y + animatedItem.top],
           });
-          const left = this.animatedValue.interpolate({
+          let left = this.animatedValue.interpolate({
             inputRange: [0, 1],
-            outputRange: [origin.x + 5, originCenter.x - animatedItem.left - 15],
+            outputRange: [origin.x + (switchButtonWidth - itemWidth) / 2, originCenter.x - animatedItem.left - itemWidth / 2],
           });
-          const rotate = this.animatedValue.interpolate({
+          let rotate = this.animatedValue.interpolate({
             inputRange: [0, 1],
             outputRange: ['0deg', '3600deg'],
           });
-          const opacity = this.animatedValue;
+          let scale = this.animatedValue.interpolate({
+            inputRange: [0, 0.2, 1],
+            outputRange: [0, 0.5, 1],
+            extrapolateRight: 'clamp',
+          });
+
+          if (isClicked) {
+            top = originCenter.y + animatedItem.top;
+            left = originCenter.x - animatedItem.left - itemWidth / 2;
+            rotate = '0deg';
+            scale = this.clickedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, animationScale],
+            });
+          }
 
           return (
             <Animated.View
               key={`Item_${index}`}
-              style={[styles.item, {top, left, transform:[{rotate}], opacity}]}
+              style={[styles.item, { top, left, transform: [{ rotate }, { scale }] }]}
             >
-              <Text style={styles.itemText}>{index}</Text>
+              <TouchableOpacity
+                activeOpacity={0.75}
+                onPress={() => this.hanldePressItem(index)}
+                style={styles.menuBtn}
+              >
+                <Text style={styles.itemText}>{index}</Text>
+              </TouchableOpacity>
             </Animated.View>
           );
         })}
@@ -159,7 +229,7 @@ export default class PathMenuAnimation extends Component {
           style={styles.centerBtn}
           onPress={this.handleShowOrHideMenu}
         >
-          <Animated.Text style={[{color: '#fff'}, {transform: [{rotate}]}]}>＋</Animated.Text>
+          <Animated.Text style={[{ color: '#fff' }, { transform: [{ rotate }] }]}>＋</Animated.Text>
         </TouchableOpacity>
       </View>
     );
