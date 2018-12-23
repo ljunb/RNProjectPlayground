4b76e5936991ab3e799330a31274cd4aeea9afc5
ljunb <cookiejlim@gmail.com>
2018-12-23 20:54:01 +0800

    [update] dealloc react bridge

--- ios/Utils/ReactRootViewManager.m
+++ ios/Utils/ReactRootViewManager.m
@@ -16,62 +16,63 @@
 @implementation ReactRootViewManager
 
 - (void)dealloc {
-	_rootViewMap = nil;
-	[_bridge invalidate];
+  _rootViewMap = nil;
+  [_bridge invalidate];
+  _bridge = nil;
 }
 
 + (instancetype)manager {
-	static ReactRootViewManager *_rootViewManager = nil;
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-		_rootViewManager = [[ReactRootViewManager alloc] init];
-	});
-	return _rootViewManager;
+  static ReactRootViewManager *_rootViewManager = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    _rootViewManager = [[ReactRootViewManager alloc] init];
+  });
+  return _rootViewManager;
 }
 
 + (instancetype)allocWithZone:(struct _NSZone *)zone {
-	static ReactRootViewManager *_rootViewManager = nil;
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-		_rootViewManager = [super allocWithZone:zone];
-	});
-	return _rootViewManager;
+  static ReactRootViewManager *_rootViewManager = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    _rootViewManager = [super allocWithZone:zone];
+  });
+  return _rootViewManager;
 }
 
 - (instancetype)init {
-	if (self = [super init]) {
-		_rootViewMap = [NSMutableDictionary dictionaryWithCapacity:0];
-		_bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
-	}
-	return self;
+  if (self = [super init]) {
+    _rootViewMap = [NSMutableDictionary dictionaryWithCapacity:0];
+    _bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
+  }
+  return self;
 }
 
 - (void)preLoadRootViewWithName:(NSString *)pageName {
-	[self preLoadRootViewWithName:pageName initialProperty:nil];
+  [self preLoadRootViewWithName:pageName initialProperty:nil];
 }
 
 - (void)preLoadRootViewWithName:(NSString *)pageName initialProperty:(NSDictionary *)initialProperty {
-	if (!pageName && [_rootViewMap objectForKey:pageName]) {
-		return;
-	}
+  if (!pageName && [_rootViewMap objectForKey:pageName]) {
+    return;
+  }
   NSMutableDictionary *tmpInitialProperty = [NSMutableDictionary dictionaryWithDictionary:initialProperty];
   [tmpInitialProperty setObject:pageName ?: @"default" forKey:PAGE_NAME_KEY];
-	// create root view with bridge
-	RCTRootView *rnView = [[RCTRootView alloc] initWithBridge:self.bridge
-																								 moduleName:APP_MODULE_NAME
-																					initialProperties:tmpInitialProperty];
-	[self.rootViewMap setObject:rnView forKey:pageName];
+  // create root view with bridge
+  RCTRootView *rnView = [[RCTRootView alloc] initWithBridge:self.bridge
+                                                 moduleName:APP_MODULE_NAME
+                                          initialProperties:tmpInitialProperty];
+  [self.rootViewMap setObject:rnView forKey:pageName];
 }
 
 - (void)removeAllPreLoadRootView {
-	[self.rootViewMap removeAllObjects];
+  [self.rootViewMap removeAllObjects];
 }
 
 - (RCTRootView *)rootViewWithName:(NSString *)pageName {
-	if (!pageName) {
-		return nil;
-	}
-	return [self.rootViewMap objectForKey:pageName];
+  if (!pageName) {
+    return nil;
+  }
+  return [self.rootViewMap objectForKey:pageName];
 }
 
 #pragma mark - RCTBridgeDelegate
